b4c6a35e6fc7dad249ca131313420bca7e967730
yaoyuan0608 <yuan.yao0608@gmail.com>
2022-10-31 23:05:42 -0700

    finish unit test

---
+++ db/query/transfer.sql
@@ -0,0 +1,29 @@
+-- name: CreateTransfer :one
+INSERT INTO transfer (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfer
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfer :many
+SELECT * FROM transfer
+WHERE from_account_id = $1 OR to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
+
+-- -- name: UpdateTransfer :one
+-- UPDATE transfer
+-- set amount = $2
+-- WHERE id = $1
+-- RETURNING *;
+
+-- -- name: DeleteTransfer :exec
+-- DELETE FROM transfer
+-- WHERE id = $1;
